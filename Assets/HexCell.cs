c7e99b64976db6b1a0dcce133672f2194e120766
Dmitriy Tabakov <nebo.reka@gmail.com>
2017-01-26 23:27:05 +0300

    First stuffing

---
+++ Assets/HexCell.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+
+public class HexCell : MonoBehaviour {
+	public HexCoordinates coordinates;
+
+	public Color Color {
+		get {
+			return color;
+		}
+		set {
+			if (color == value) {
+				return;
+			}
+			color = value;
+			Refresh();
+		}
+	}
+
+	Color color;
+
+	[SerializeField]
+	HexCell[] neighbors;
+
+	int elevation = int.MinValue;
+
+	public RectTransform uiRect;
+
+	public HexGridChunk chunk;
+
+	public Vector3 Position {
+		get {
+			return transform.localPosition;
+		}
+	}
+
+	void Refresh () {
+		if (chunk) {
+			chunk.Refresh();
+			for (int i = 0; i < neighbors.Length; i++) {
+				HexCell neighbor = neighbors[i];
+				if (neighbor != null && neighbor.chunk != chunk) {
+					neighbor.chunk.Refresh();
+				}
+			}
+		}
+	}
+
+	public int Elevation {
+		get {
+			return elevation;
+		}
+		set {
+			if (elevation == value) {
+				return;
+			}
+
+			elevation = value;
+			Vector3 position = transform.localPosition;
+			position.y = value * HexMetrics.elevationStep;
+			position.y +=
+				(HexMetrics.SampleNoise(position).y * 2f - 1f) *
+				HexMetrics.elevationPerturbStrength;
+			transform.localPosition = position;
+
+			Vector3 uiPosition = uiRect.localPosition;
+			uiPosition.z = -position.y;
+			uiRect.localPosition = uiPosition;
+
+			Refresh();
+		}
+	}
+
+	public HexEdgeType GetEdgeType (HexDirection direction) {
+		return HexMetrics.GetEdgeType(
+			elevation, neighbors[(int)direction].elevation
+		);
+	}
+
+	public HexEdgeType GetEdgeType (HexCell otherCell) {
+		return HexMetrics.GetEdgeType(
+			elevation, otherCell.elevation
+		);
+	}
+
+	public HexCell GetNeighbor (HexDirection direction) {
+		return neighbors[(int)direction];
+	}
+
+	public void SetNeighbor (HexDirection direction, HexCell cell) {
+		neighbors[(int)direction] = cell;
+		cell.neighbors[(int)direction.Opposite()] = this;
+	}
+}
---
+++ Assets/HexMapEditor.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HexMapEditor : MonoBehaviour {
+
+	public Color[] colors;
+
+	public HexGrid hexGrid;
+
+	private Color activeColor;
+
+	int activeElevation;
+
+	bool applyColor;
+
+	bool applyElevation = true;
+
+	void Awake () {
+		SelectColor(0);
+	}
+
+	void Update () {
+		if (Input.GetMouseButton(0) &&
+			!EventSystem.current.IsPointerOverGameObject()) {
+			HandleInput();
+		}
+	}
+
+	void HandleInput () {
+		Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast(inputRay, out hit)) {
+			EditCell(hexGrid.GetCell(hit.point));
+		}
+	}
+
+	public void SelectColor (int index) {
+		applyColor = index >= 0;
+		if (applyColor) {
+			activeColor = colors[index];
+		}
+	}	
+
+	void EditCell (HexCell cell) {
+		if (applyColor) {
+			cell.Color = activeColor;
+		}
+		if (applyElevation) {
+			cell.Elevation = activeElevation;
+		}
+	}
+
+	public void SetElevation (float elevation) {
+		activeElevation = (int)elevation;
+	}
+
+	public void SetApplyElevation (bool toggle) {
+		applyElevation = toggle;
+	}
+}
